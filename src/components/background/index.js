2272a8271f982e4ebd2720b7591691df7ba1adf6
Xunnamius (Morty) <Xunnamius@users.noreply.github.com>
2018-09-23 17:30:17 -0500

    incremental

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -1,4 +1,12 @@
 /* import './img/icon-128.png'
 import './img/icon-34.png' */
 
-console.log('hello, world!');
+import http from 'faxios'
+
+const DNS_TARGET_FQDN_URI = 'bernarddickens.com';
+const GOOGLE_DNS_TARGET_URI = (resourceHash1, resourceHash2, targetDomain) =>
+    `https://dns.google.com/resolve?name=${resourceHash1}.${resourceHash2}._dnschk.${targetDomain}&type=TXT`;
+
+chrome.downloads.onCreated.addListener(item => console.log('downloads.onCreated listener called!', item));
+
+
