af98c625540788b9e05fa4cd961d232ff11f0338
Xunnamius (Morty) <Xunnamius@users.noreply.github.com>
2018-09-24 16:23:33 -0500

    incremental

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -7,6 +7,7 @@ const DNS_TARGET_FQDN_URI = 'bernarddickens.com';
 const GOOGLE_DNS_TARGET_URI = (resourceHash1, resourceHash2, targetDomain) =>
     `https://dns.google.com/resolve?name=${resourceHash1}.${resourceHash2}._dnschk.${targetDomain}&type=TXT`;
 
-chrome.downloads.onCreated.addListener(item => console.log('downloads.onCreated listener called!', item));
+// ? This event fires with the DownloadItem object when a download begins
+chrome.downloads.onCreated.addListener(downloadItem => console.log('downloads.onCreated listener called!', downloadItem));
 
 
